384541ab96ee504e407577e960376a0622914743
BorisLipatnikov <iriisc@yandex.ru>
2026-04-27 16:08:42 +0300

    Add files via upload

---
+++ accountant.cpp
@@ -0,0 +1,18 @@
+/*
+#include "accountant.h"
+
+#include "square_calculation.h"
+
+using namespace std;
+
+template <class BuildingObject>
+double Accountant::CalcPaintNeeded(const BuildingObject& building_object) const {
+    
+}
+
+template <class BuildingObject>
+double Accountant::CalcBricksNeeded(const BuildingObject& building_object) const {
+
+}
+    
+    */
